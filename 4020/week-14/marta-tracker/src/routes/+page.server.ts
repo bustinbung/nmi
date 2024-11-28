9c8cc40ae8653eaf5c984bced3339d4b5735371a
Justin Jung <justin@bustinbung.com>
2024-11-27 19:46:47 -0500

    Push to build on Vercel

---
+++ 4020/week-14/marta-tracker/src/routes/+page.server.ts
@@ -0,0 +1,7 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from "./$types"
+
+export const load: PageServerLoad = async ({ params }) => {
+    const response = fetch('https://gtfs-rt.itsmarta.com/TMGTFSRealTimeWebService/vehicle/vehiclepositions.pb');
+    console.log(response);
+}
---
+++ 4020/week-14/marta-tracker/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang=ts>
+    // import Pbf from 'pbf';
+    // import { readAlert } from '$lib/gtfs-proto-schema';
+
+    // try {
+    //     const response = fetch('https://gtfs-rt.itsmarta.com/TMGTFSRealTimeWebService/vehicle/vehiclepositions.pb');
+
+    //     console.log(response);
+    // } catch {
+    //     throw new Error('Failed');
+    // }
+
+    // let obj = readAlert(new Pbf());
+</script>
+<h1>MARTA Real-time Transit Information</h1>
